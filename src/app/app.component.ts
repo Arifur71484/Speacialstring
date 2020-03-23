27f44d42e7baca5ab4bc05fb0a98ea507f281540
Arifur Rahman <arifur.rahman@studenti.unicam.it>
2020-03-23 11:55:11 +0100

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,97 @@
-import { Component } from '@angular/core';
+import { Component , OnInit , OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { InjectableRxStompConfig, RxStompService } from '@stomp/ng2-stompjs';
+import { Subscription } from 'rxjs';
+ 
+import { Message } from '@stomp/stompjs';
+
+import { myRxStompConfig } from './my-rx-stomp.config'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'speacialstring';
+export class AppComponent implements OnInit , OnDestroy {
+  title = 'stringapp';
+   
+  private topicSubscription : Subscription ;
+
+  New : string ;
+
+  ReverseNew : String ;
+  ReverseNN : String ;
+ 
+  urlBroker : string ;
+  userName  : string ;
+  passcode : string ;
+  topic : string ;
+  brokerURL : string ;
+  reverseString : string ;
+ 
+ 
+
+    constructor (public rxStompService: RxStompService )   {
+          
+      this.urlBroker = myRxStompConfig.brokerURL
+      this.userName = myRxStompConfig.connectHeaders.login
+      this.passcode = myRxStompConfig.connectHeaders.passcode
+      this.topic =   "something"
+      this.topicSubscription =  null;
+    }
+
+
+    ngOnInit () {
+      console.log("ngOnInit");
+      this.rxStompService.deactivate()
+  
+      this.rxStompService.configure ({
+        brokerURL : this.urlBroker ,
+        connectHeaders: {
+          login: this.userName,
+          passcode: this.passcode
+        },
+        heartbeatIncoming: 0,
+        heartbeatOutgoing: 20000,
+        reconnectDelay: 5000,
+      })
+
+      this.rxStompService.activate();
+
+      this.topicSubscription = this.rxStompService.watch( this.topic ).subscribe (
+        (message: Message) => {    
+          console.log("message.body", message.body); 
+          this.ReverseNew = JSON.parse(message.body).value;
+
+        },
+        (err) => {
+          console.log(err);
+        }
+      ); 
+    }
+
+
+  onSignUsers(form: NgForm)  {
+    const Newname= form.value.name;     
+    this.New =  this.reverse(Newname);
+    this.ReverseNN  = this.ReverseNew ;
+    
+    console.log(this.New);
+  }
+
+    ngOnDestroy () {
+      if (this.topicSubscription != null)
+      
+           this.topicSubscription.unsubscribe() ;
+    }
+
+      reverse(str:string) {
+        var str1 = "$£"; 
+        var str2=   str.split('').reverse().join(''); 
+        var str3 ="£$"; 
+         var res= str1.concat( str2, str3 ); 
+        return res;
+      
+       }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule}  from '@angular/forms';
 import { AppComponent } from './app.component';
+import { InjectableRxStompConfig, RxStompService, rxStompServiceFactory } from '@stomp/ng2-stompjs';
+import { myRxStompConfig } from './my-rx-stomp.config';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    {
+      provide: InjectableRxStompConfig,
+      useValue: myRxStompConfig
+      },
+      {
+      provide: RxStompService,
+      useFactory: rxStompServiceFactory,
+      deps: [InjectableRxStompConfig]
+      }
+
+
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
